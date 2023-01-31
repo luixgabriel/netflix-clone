2f6508b7394bdf2665d749027b053e106d990fa9
luix gabriel <luisgasbriel@gmail.com>
2023-01-31 09:33:23 -0300

    finishing lists

--- src/components/movieRow/movieRow.js
+++ src/components/movieRow/movieRow.js
@@ -1,3 +1,4 @@
+/* eslint-disable import/no-extraneous-dependencies */
 import React from 'react';
 import './Movierow.css';
 import PropTypes from 'prop-types';
@@ -5,7 +6,17 @@ import PropTypes from 'prop-types';
 export default function ListMovies({ title, itens }) {
   return (
     <div className="movieList">
-      <h1>{title}</h1>
+      <h2>{title}</h2>
+      <div className="movieList--area">
+        <div className="movielist--list">
+          {itens.results.length > 0 && itens.results.map((movie) => (
+            <div key={movie.id} className="movielist--item">
+              <img src={`https://image.tmdb.org/t/p/w300${movie.poster_path}`} alt={movie.original_title} />
+            </div>
+
+          ))}
+        </div>
+      </div>
     </div>
   );
 }
